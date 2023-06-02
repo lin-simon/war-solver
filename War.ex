3e4a1ff13b2647e7ffead6d8052fc9a64dd9d37b
lin-simon <88695972+lin-simon@users.noreply.github.com>
2023-06-02 13:36:44 -0400

    Update War.ex

--- War.ex
+++ War.ex
@@ -1,18 +1,4 @@
 defmodule War do
-  @moduledoc """
-  	Documentation for `War`.
-  """
-
-  @doc """
-  	Function stub for deal/1 is given below. Feel free to add
-  	as many additional helper functions as you want.
-
-  	The tests for the deal function can be found in test/war_test.exs.
-  	You can add your five test cases to this file.
-
-  	Run the tester by executing 'mix test' from the war directory
-  	(the one containing mix .exs)
-  """
  # populate hand with even indices of shuf
   def deal_helper_1(shuf) do
     Enum.with_index(shuf)
